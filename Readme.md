a4e60bac3174a50f5b16436d20bd121f0c5026b2
latifkara <latifkara93@gmail.com>
2022-11-23 22:56:13 +0300

    Selenium case Project

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Ebebek odevi
+
+1. Menual Test
+
+Sepet sayfasındaki menual test aşağıdaki gibi yapılmıştır.
+<video width="320" height="240" controls>
+  <source src="Sepet_ebebek.mp4" type="video/mp4">
+</video>
+
+2. Test Otomasyonu (UI)
+
+Selenium teknolojisini kullanarak test otomasyon işlemi gerçekleştirilmiştir.
+
+3. Test Otomasyonu – API
+Java Maven ile test işlemi yapılmış ve başarılı ile başarsız istekler alınmıştır.
+   
+
+4. Hata Raporlama 
---
+++ src/com/javaGelisme/TestOtomationUI.java
@@ -0,0 +1,46 @@
+package com.javaGelisme;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.security.Key;
+
+public class TestOtomationUI {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/latifkara/Selenium/ChromeDriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.e-bebek.com/");
+        driver.manage().window().maximize();
+
+        WebElement search = driver.findElement(By.id("txtSearchBox"));
+        search.sendKeys("biberon");
+        Thread.sleep(1000);
+
+        search.sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+
+        WebElement firstProduct = driver.findElement(By.className("product-item"));
+        firstProduct.click();
+        Thread.sleep(2000);
+
+        WebElement productButton = driver.findElement(By.id("addToCartBtn"));
+        productButton.click();
+        Thread.sleep(2000);
+
+        WebElement showProduct = driver.findElement(By.id("btnShowCart"));
+        showProduct.click();
+
+
+
+
+
+
+
+    }
+}
